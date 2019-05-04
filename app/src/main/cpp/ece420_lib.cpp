b41fe75d54d898ca293a2caffed77956f2ef2482
seedship☄️ <rnai2@illinois.edu>
2019-05-03 22:01:48 -0500

    commenting lib.cpp

--- app/src/main/cpp/ece420_lib.cpp
+++ app/src/main/cpp/ece420_lib.cpp
@@ -1,23 +1,30 @@
-
 #include <cmath>
 #include "ece420_lib.h"
 #include <opencv2/ml/ml.hpp>
 
+//This is a function to convert from the frequency domain to the mel frequency domain.
 double HzToMel(double freq)
 {
 	return 2595 * log10( (freq / 700.0) + 1);
 }
 
+//This is a function to convert from the mel frequency domain to the frequency domain.
 double MelToHz(double mel)
 {
 	return (pow(10, mel / 2595.0) - 1) * 700;
 }
 
+//This function creates the filter banks for the mel frequency coefficient vectors.
 std::vector<std::vector<double>> filter_bank(unsigned num_filters, unsigned nfft, const std::vector<unsigned> &bin_points)
 {
+	//set up the filter dimensions
 	unsigned filter_length = nfft/2 + 1;
+
 	std::vector<std::vector<double>> ans(num_filters);
 
+	//This part is applying triangular filters to the frequency domain signal. If we were to take a
+	//histogram of all of the frequency components of a person's speech signal. We see that this filter bank
+	//applies a each filter to equal sized histogram bins.
 	for(unsigned j = 0; j < num_filters; j++){
 		std::vector<double> subfilter = std::vector<double>(filter_length, 0.0);
 		for(auto i = bin_points[j]; i < bin_points[j+1]; i++){
@@ -32,6 +39,7 @@ std::vector<std::vector<double>> filter_bank(unsigned num_filters, unsigned nfft
 	return ans;
 }
 
+//generate the mel frequency points based offf of the results of the filtered fequency points.
 std::vector<double> generateMelPoints(unsigned num_filters, unsigned nfft, unsigned fs)
 {
 	double highmel = HzToMel(fs/2.0);
@@ -44,6 +52,7 @@ std::vector<double> generateMelPoints(unsigned num_filters, unsigned nfft, unsig
 	return ans;
 }
 
+//helper function to get the "histogram" bins that were spoken about earlier
 std::vector<unsigned> generateBinPoints(const std::vector<double> &mel_points, unsigned nfft, unsigned fs)
 {
 	std::vector<unsigned> ans(mel_points.size());
@@ -53,6 +62,7 @@ std::vector<unsigned> generateBinPoints(const std::vector<double> &mel_points, u
 	return ans;
 }
 
+//another helper function used to get the total energy in a signal of length "length"
 double calculateEnergySquared(const kiss_fft_cpx *data_squared, unsigned length)
 {
 	double total = 0;
@@ -63,12 +73,16 @@ double calculateEnergySquared(const kiss_fft_cpx *data_squared, unsigned length)
 	return total;
 }
 
+//check if the sample speech signal is voiced
+//we learned to do this by comparing the input power to a lower bound
 bool isVoiced(const kiss_fft_cpx *data_squared, unsigned length, double threshold)
 {
 	double total = calculateEnergySquared(data_squared, length);
 	return total > threshold;
 }
 
+//this is an output of the knn classification. We detect what is closest to the input frequency vector
+//in our code book and output that as a double which is mapped to a name in a dictionary in a different file.
 double setLastFreqDetected(){
 	cv::Ptr<cv::ml::KNearest> knn = cv::ml::KNearest::create();
 	knn->setDefaultK(1);
@@ -85,6 +99,7 @@ double setLastFreqDetected(){
 	return knn->findNearest(testFeature, 1, cv::noArray());
 }
 
+//this longer function converts the sample to it's MFCC equivalent coefficient vector
 std::vector<double> sampleToMFCC(const std::vector<double> &inputData, const std::vector<std::vector<double>> &fbank)
 {
 	unsigned framesize = inputData.size();
@@ -133,34 +148,36 @@ std::vector<double> sampleToMFCC(const std::vector<double> &inputData, const std
 	return mfcc;
 }
 
+//this is another longer function that converts the sample to it's MFCC equivalent coefficient vector
 std::vector<float> sampleToMFCC(kiss_fft_cpx *data, const std::vector<std::vector<double> > &fbank, unsigned nfft)
 {
 
-    for(unsigned x = 0; x < nfft; x++){
-        data[x].r /= nfft;
-    }
+	for(unsigned x = 0; x < nfft; x++){
+		data[x].r /= nfft;
+	}
 
-    std::vector<float> mfcc(20);
-    for(unsigned x = 0; x < fbank.size(); x++){
-        float total = 0;
-        for(unsigned y = 0; y < fbank[0].size(); y++){
-            total += data[y].r * fbank[x][y];
-        }
-        mfcc[x] = log(total + 0.001f);
-    }
+	std::vector<float> mfcc(20);
+	for(unsigned x = 0; x < fbank.size(); x++){
+		float total = 0;
+		for(unsigned y = 0; y < fbank[0].size(); y++){
+			total += data[y].r * fbank[x][y];
+		}
+		mfcc[x] = log(total + 0.001f);
+	}
 
-    mfcc = naiveDCT(mfcc);
-    for(unsigned x = 0; x < 20; x++){
-        if(!x)
-            mfcc[x] = mfcc[x] * sqrt(1.0f/(4*20));
-        else {
-            mfcc[x] = mfcc[x] * sqrt(1.0f/(2*20));
-        }
-    }
+	mfcc = naiveDCT(mfcc);
+	for(unsigned x = 0; x < 20; x++){
+		if(!x)
+			mfcc[x] = mfcc[x] * sqrt(1.0f/(4*20));
+		else {
+			mfcc[x] = mfcc[x] * sqrt(1.0f/(2*20));
+		}
+	}
 
-    return mfcc;
+	return mfcc;
 }
 
+//this function computes the type 4 Discrete Cosine Transform on an input signal.
 std::vector<double> naiveDCT(const std::vector<double> &input)
 {
 	unsigned N = input.size();
@@ -175,20 +192,23 @@ std::vector<double> naiveDCT(const std::vector<double> &input)
 	return ans;
 }
 
+//this function computes the type 2 Discrete Cosine Transform on an input signal.
 std::vector<float> naiveDCT(const std::vector<float> &input)
 {
-    unsigned N = input.size();
-    std::vector<float> ans(N);
-    for(unsigned k = 0; k < N; k++){
-        double sum = 0;
-        for(unsigned n = 0; n < N; n++){
-            sum += 2* input[n] * cos(M_PI * k * double(2*n+1)/(2*N));
-        }
-        ans[k] = sum;
-    }
-    return ans;
+	unsigned N = input.size();
+	std::vector<float> ans(N);
+	for(unsigned k = 0; k < N; k++){
+		double sum = 0;
+		for(unsigned n = 0; n < N; n++){
+			sum += 2* input[n] * cos(M_PI * k * double(2*n+1)/(2*N));
+		}
+		ans[k] = sum;
+	}
+	return ans;
 }
 
+//we need to recreate the knn classifier every time we need to get new input vectors
+// this function does just that.
 void updateKNN(const std::vector<int> & labels, const std::vector<float> & vectors, cv::Ptr<cv::ml::KNearest> &knn)
 {
 	unsigned num_vectors = (unsigned)labels.size();
